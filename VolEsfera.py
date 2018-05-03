207043d50333d952957aaa0a6798869f0206d921
Usuario-PC\Usuario <diana.narvaez@epn.edu.ec>
2018-05-02 22:39:12 -0500

    Calculo de Volumen de una esfera

---
+++ VolEsfera.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 29 23:06:19 2018
+
+@author: José Cortez
+"""
+import math# Biblioteca para operaciones matemáticas
+
+#Ingreso de datos
+print("**********Cálculo del volumen de una esfera**********")
+R=float(input("Ingrese el valor radio en centimetros\n"))
+
+#Funcion que calcula el volumen de la esfera
+def volEsfera(R):
+    
+    # Cálculo del volumen de la figura
+    volumen=((4/3)*(math.pi)*(R**3))
+    
+    # Muestra en consola el resultado del volumen
+    print("El volumen de la Esfera es = "+str(volumen)+" Cm cúbicos ")
+    
+# Llamado a la función que cálcula de la esfera
+volEsfera(R)
